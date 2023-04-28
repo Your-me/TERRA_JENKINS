05235e4a29cd5266f0c7a1708149031709745b9b
Your-me <akinmoladunabayomi@gmail.com>
2023-04-28 11:42:17 +0100

    updated

--- dev/variables.tf
+++ dev/variables.tf
@@ -1,4 +1,12 @@
-variable "vpc_cidr_block" {}
-variable "pub_cidr_block" {}
-variable "priv_cidr_block" {}
-variable "region" {}
+variable "vpc_cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "pub_cidr_block" {
+    default = "10.0.1.0/24"
+}
+variable "priv_cidr_block" {
+    default = "10.0.2.0/24"
+}
+variable "region" {
+    default ="eu-west-2"
+}
