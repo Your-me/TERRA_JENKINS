759cb478cd47a3b9e92a748581e71b112e36117a
Your-me <akinmoladunabayomi@gmail.com>
2023-04-28 04:31:14 +0100

    updated

--- dev/main.tf
+++ dev/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 #Create VPC
 module "vpc" {
-  source          = "../vpc/"
+  source          = "./vpc/"
   vpc_cidr_block  = var.vpc_cidr_block
   pub_cidr_block  = var.pub_cidr_block
   priv_cidr_block = var.priv_cidr_block
