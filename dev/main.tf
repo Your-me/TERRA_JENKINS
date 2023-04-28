980442b62f055799944f2c634e308993afb6245a
Your-me <akinmoladunabayomi@gmail.com>
2023-04-28 04:12:30 +0100

    updated

--- dev/main.tf
+++ dev/main.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-    region = var.region
+  region = var.region
 }
 
 #Create VPC
 module "vpc" {
-  source = "../vpc/"
-  vpc_cidr_block = var.vpc_cidr_block
-  pub_cidr_block = var.pub_cidr_block
+  source          = "../vpc/"
+  vpc_cidr_block  = var.vpc_cidr_block
+  pub_cidr_block  = var.pub_cidr_block
   priv_cidr_block = var.priv_cidr_block
-  region = var.region
+  region          = var.region
 
 
 
--- dev/terraform.tfvars
+++ dev/terraform.tfvars
@@ -1,4 +1,4 @@
-vpc_cidr_block="10.0.0.0/16"
-pub_cidr_block="10.0.1.0/24"
-priv_cidr_block="10.0.2.0/24"
-region="eu-west-2"
+vpc_cidr_block  = "10.0.0.0/16"
+pub_cidr_block  = "10.0.1.0/24"
+priv_cidr_block = "10.0.2.0/24"
+region          = "eu-west-2"
